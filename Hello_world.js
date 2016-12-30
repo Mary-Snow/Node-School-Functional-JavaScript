b664748ff4da593e234392f2f20f73877d0c4031
Mary-Snow <snow.mary.r@gmail.com>
2016-12-30 10:19:00 -0800

    Created a file with the first problem and solution

---
+++ Hello_world.js
@@ -0,0 +1,7 @@
+/*
+  Write a function that takes an input string and returns it uppercases
+*/function upperCaser(input) {
+      return input.toUpperCase();
+    }
+
+    module.exports = upperCaser
